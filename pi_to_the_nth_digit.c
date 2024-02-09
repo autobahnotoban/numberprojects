01cdac54bb9d7d60b29dd9b95d4e0a5d101de002
autobahnotoban <arabaciatahan@gmail.com>
2024-02-09 23:18:58 +0300

    more stuff

---
+++ pi_to_the_nth_digit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main ()
+{
+    int digit;
+    printf("Until which digit of pi after the comma would you like to see?\n");
+    scanf("%d",&digit);
+
+    float pi = (22/7);
+
+    printf("%f",pi);
+
+}
